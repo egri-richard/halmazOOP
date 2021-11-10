9701d3e7f1cf86b3e6bbd217d1944208de90cf02
egri-richard <egri.dani.r@gmail.com>
2021-11-10 16:06:00 +0100

    Szobakerekpar

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
+import com.company.Exercise.Szobakerekpar;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Szobakerekpar.Run();
     }
 }
